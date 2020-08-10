df8fb8b81277157f12b708ac29e7a3d491e278f0
Maria Membreno <alegria10maria@gmail.com>
2020-08-10 16:44:10 -0600

    change readme

--- app/Http/Controllers/API/CovidController.php
+++ app/Http/Controllers/API/CovidController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Exception;
 use Illuminate\Support\Facades\Http;
 
 class CovidController extends Controller
@@ -22,13 +23,13 @@ class CovidController extends Controller
             'x-rapidapi-host' => 'covid-193.p.rapidapi.com',
             'x-rapidapi-key' => '03b4755197mshcb4b0f5c939eee6p12fc8ejsncc5b7b48c4b9',
             'useQueryString' => true
-        ])->get('https://covid-193.p.rapidapi.com/countries');
+        ])->get('https://covid-193.p.rapidapi.com/statistics');
         if ($response->ok()) {
 
             foreach ($response->json()['response'] as $c) {
                 $this->countries[] = [
-                    "code" => $c,
-                    "name" => str_replace("-"," ",$c)
+                    "code" => $c['country'],
+                    "name" => str_replace("-"," ",$c['country'])
                 ];
             }
         }
@@ -49,21 +50,97 @@ class CovidController extends Controller
             'useQueryString'=>true
         ])->get("https://covid-193.p.rapidapi.com/statistics", $args);
 
-        return $response;
+        $_data=[];
+
+        if ($response->ok()) {
+
+            foreach ($response->json()['response'] as $c) {
+                if($c['continent'] == 'North-America' || $c['continent'] == 'South-America' || empty($c['continent'])){
+                    $c['continent'] = "America";
+                }
+                if($c['country'] == $c['continent']){
+                    continue;
+                }
+
+                $date = \Carbon\Carbon::now();
+                $createdat = \Carbon\Carbon::parse($c['time']);
+                $diff_month =  $createdat->diffInMonths($date);
+                $diff_weeks =  $createdat->diffInWeeks($date);
+                $diff_days =  $createdat->diffInDays($date);
+                $diff_hours =  $createdat->diffInHours($date);
+                $diff_minutes =  $createdat->diffInMinutes($date);
+                $diff_seconds =  $createdat->diffInMinutes($date);
+
+                $time_string="";
+
+                if ($diff_month > 0) {
+
+                    if ($diff_month == 1) {
+                        $time_string = "1 month ago";
+                    } else {
+                        $time_string ="$diff_month months ago";
+                    }
+                } elseif ($diff_weeks > 0) {
+
+                    if ($diff_weeks == 1) {
+                        $time_string =  "1 week ago";
+                    } else {
+                        $time_string =  "$diff_weeks weeks ago";
+                    }
+                } elseif ($diff_days > 0) {
+
+                    if ($diff_days == 1) {
+                        $time_string =  "a day ago";
+                    } else {
+                        $time_string =  "$diff_days days ago";
+                    }
+                } elseif ($diff_minutes > 0) {
+
+                    if ($diff_minutes == 1) {
+                        $time_string =  "a minutes ago";
+                    } else {
+                        $time_string =  "$diff_minutes minutes ago";
+                    }
+                } elseif ($diff_seconds > 0) {
+
+                    if ($diff_seconds == 1) {
+                        $time_string =  "a second ago";
+                    } else {
+                        $time_string =  "$diff_seconds seconds ago";
+                    }
+                }
+
+
+                $_data[] = [
+                    "code" => $c['country'],
+                    "country" => str_replace("-", " ", $c['country']),
+                    "population" => $c['population'],
+                    "cases" => $c['cases']['total'],
+                    "time"=> $time_string,
+                    'continent'=> $c['continent']
+                ];
+            }
+        }
+
+        return $_data;
 
     }
 
     public function index()
     {
-        $covid_data = $this->getCovidAPI();
+        try{
+            $covid_data = $this->getCovidAPI();
 
-        $data=['covid_data'=>json_encode([]),'countries'=> $this->countries];
-
-        if($covid_data->ok()){
-            $data['covid_data']= $covid_data->json();
+            $data = ['covid_data' => $covid_data];
+            if (empty($covid_data)) {
+                throw new Exception("There's no data to show");
+            }
+            return response()->json($data);
+        }
+        catch(Exception $ex){
+            throw new Exception($ex->getMessage());
         }
 
-        return response()->json($data);
     }
 
     /**
@@ -83,21 +160,27 @@ class CovidController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($country)
+    public function show($covid)
     {
-        if(!empty($country)){
-            $api = $this->getCovidAPI($country);
+        try{
+            if (!empty($covid)) {
+                $api = $this->getCovidAPI($covid);
 
-            if($api->ok()){
-                return response()->json(["covid_api"=>$api->json()]);
-            }
-            else{
-                return response()->json(["message" => "The request is failed!"]);
+                $data = ['covid_data' => $api];
+
+                if (empty($api)) {
+                    throw new Exception("There's no data to show");
+                }
+
+                return response()->json($data);
+            } else {
+                throw new Exception("The field country is required");
             }
         }
-        else{
-            return response()->json(["message" => "The field country is required"]);
+        catch(Exception $ex){
+            throw new Exception($ex->getMessage());
         }
+
     }
 
     /**
